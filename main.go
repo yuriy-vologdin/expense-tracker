086b4afbdbcd676e32b076f6c3d40f5d9f4d93b6
Yuriy Vologdin <yuriy-vologdin07@mail.ru>
2026-04-19 23:58:04 +0300

    Completed basic logic: add, list, delete and summary

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
+	"strconv"
 	"time"
 )
 
@@ -49,7 +50,29 @@ func load() (storage, error) {
 
 		return storage{Nextid: 1}, fmt.Errorf("File unmarshal error: %v", err)
 	}
+
 	return s, nil
+}
+
+func add(s *storage, amount int, description string) {
+	var exp expense
+	exp.ID = s.Nextid
+	exp.Date = time.Now()
+	exp.Amount = amount
+	exp.Description = description
+	s.Expenses = append(s.Expenses, exp)
+	s.Nextid++
+	fmt.Printf("Expense added successfully (ID: %d)\n", s.Nextid)
+}
+
+func delete(s *storage, id int) {
+	newExpences := make([]expense, 0)
+	for _, exp := range s.Expenses {
+		if exp.ID != id {
+			newExpences = append(newExpences, exp)
+		}
+	}
+	s.Expenses = newExpences
 }
 
 func main() {
@@ -57,10 +80,48 @@ func main() {
 	if err != nil {
 		fmt.Println(err)
 	}
-	if len(os.Args) > 1 && os.Args[1] == "list" {
-		for _, exp := range data.Expenses {
-			fmt.Printf("%-4d %-20s %-5d %s\n", exp.ID,
-				exp.Date.Format("02/01/2006 15:04"), exp.Amount, exp.Description)
+	if len(os.Args) > 1 {
+		switch os.Args[1] {
+		case "list":
+			for _, exp := range data.Expenses {
+				fmt.Printf("%-4d %-12s %-5d %s\n", exp.ID,
+					exp.Date.Format("02-01-2006"), exp.Amount, exp.Description)
+			}
+		case "add":
+			if len(os.Args) != 4 {
+				fmt.Printf("Error: invalid number of arguments, use add \"description\" amount\n")
+			} else {
+				amount, err := strconv.Atoi(os.Args[3])
+				if err != nil {
+					fmt.Printf("Error: incorrect amount format, amount must be integer\n")
+				}
+				description := os.Args[2]
+				add(&data, amount, description)
+				save(data)
+			}
+		case "summary":
+			var sum int
+			for _, exp := range data.Expenses {
+				sum += exp.Amount
+			}
+			fmt.Printf("Total expences: %d\n", sum)
+		case "delete":
+			if len(os.Args) != 3 {
+				fmt.Printf("Error: %v, use delete id\n", err)
+			} else {
+				id, err := strconv.Atoi(os.Args[2])
+				if err != nil {
+					fmt.Printf("Error: incorrect ID format, ID must be integer\n")
+				}
+				oldLen := len(data.Expenses)
+				delete(&data, id)
+				save(data)
+				if oldLen == len(data.Expenses) {
+					fmt.Printf("No expense with ID %d\n", id)
+				} else {
+					fmt.Printf("Expense deleted successfully\n")
+				}
+			}
 		}
 	}
 
