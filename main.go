648d395e4c10509a6e20429c9b38176588ee00fa
Yuriy Vologdin <yuriy-vologdin07@mail.ru>
2026-04-23 18:30:02 +0300

    Refactor: convert functions to methods and optimize summary logic

--- main.go
+++ main.go
@@ -22,12 +22,12 @@ type storage struct {
 	Expenses []expense `json:"expenses"`
 }
 
-func save(s storage) error {
+func (s storage) save() error {
 	jsonData, err := json.MarshalIndent(s, "", "  ")
 	if err != nil {
-
 		return fmt.Errorf("Marshaling error: %v", err)
 	}
+
 	os.WriteFile(filename, jsonData, 0644)
 
 	return nil
@@ -35,94 +35,137 @@ func save(s storage) error {
 
 func load() (storage, error) {
 	_, err := os.Stat(filename)
-	if err != nil {
 
+	if err != nil {
 		return storage{Nextid: 1}, nil
 	}
 	data, err := os.ReadFile(filename)
 	if err != nil {
-
 		return storage{Nextid: 1}, fmt.Errorf("File reading error: %v", err)
 	}
+
 	var s storage
 	err = json.Unmarshal(data, &s)
 	if err != nil {
-
 		return storage{Nextid: 1}, fmt.Errorf("File unmarshal error: %v", err)
 	}
 
 	return s, nil
 }
 
-func add(s *storage, amount int, description string) {
-	var exp expense
-	exp.ID = s.Nextid
-	exp.Date = time.Now()
-	exp.Amount = amount
-	exp.Description = description
-	s.Expenses = append(s.Expenses, exp)
-	s.Nextid++
-	fmt.Printf("Expense added successfully (ID: %d)\n", s.Nextid)
+func (s *storage) list() {
+	for _, exp := range s.Expenses {
+		fmt.Printf("%-4d %-12s %-5d %s\n", exp.ID,
+			exp.Date.Format("02-01-2006"), exp.Amount, exp.Description)
+	}
 }
 
-func delete(s *storage, id int) {
-	newExpences := make([]expense, 0)
-	for _, exp := range s.Expenses {
-		if exp.ID != id {
-			newExpences = append(newExpences, exp)
+func (s *storage) add() {
+	var exp expense
+	if len(os.Args) != 4 {
+		fmt.Printf("Error: invalid number of arguments, use add \"description\" amount\n")
+	} else {
+		amount, err := strconv.Atoi(os.Args[3])
+		if err != nil {
+			fmt.Printf("Error: incorrect amount format, amount must be integer\n")
+		}
+
+		description := os.Args[2]
+		exp.ID = s.Nextid
+		exp.Date = time.Now()
+		exp.Amount = amount
+		exp.Description = description
+
+		s.Expenses = append(s.Expenses, exp)
+		fmt.Printf("Expense added successfully (ID: %d)\n", s.Nextid)
+		s.Nextid++
+		s.save()
+	}
+}
+
+func (s *storage) summary() {
+	var sum int
+	if len(os.Args) == 2 {
+		for _, exp := range s.Expenses {
+			sum += exp.Amount
+		}
+		fmt.Printf("Total expenses: %d\n", sum)
+
+	} else if len(os.Args) == 3 {
+		var month string
+		var monthNumber int
+		months := []string{"January", "February", "March", "April",
+			"May", "June", "July", "August", "September",
+			"October", "November", "December"}
+		monthIndx, err := strconv.Atoi(os.Args[2])
+
+		if err == nil && monthIndx >= 1 && monthIndx <= 12 {
+			month = months[monthIndx-1]
+			monthNumber = monthIndx
+		} else {
+			for i, monthName := range months {
+				if monthName == os.Args[2] {
+					month = monthName
+					monthNumber = i + 1
+					break
+				}
+			}
+		}
+
+		if month != "" {
+			for _, exp := range s.Expenses {
+				if int(exp.Date.Month()) == monthNumber {
+					sum += exp.Amount
+				}
+			}
+			fmt.Printf("Total expenses for %s: %d\n", month, sum)
+		} else {
+			fmt.Println(`Error: invalid month name or number, use "summary [number of month]" or "summary [name of month]"`)
 		}
 	}
-	s.Expenses = newExpences
+}
+
+func (s *storage) delete() {
+	newExpenses := make([]expense, 0)
+	if len(os.Args) != 3 {
+		fmt.Printf("Error: invalid number of arguments, use delete id\n")
+	} else {
+		id, err := strconv.Atoi(os.Args[2])
+		if err != nil {
+			fmt.Printf("Error: incorrect ID format, ID must be integer\n")
+		}
+		oldLen := len(s.Expenses)
+		for _, exp := range s.Expenses {
+			if exp.ID != id {
+				newExpenses = append(newExpenses, exp)
+			}
+		}
+		s.Expenses = newExpenses
+		if oldLen == len(s.Expenses) {
+			fmt.Printf("No expense with ID %d\n", id)
+		} else {
+			s.save()
+			fmt.Printf("Expense deleted successfully\n")
+		}
+	}
 }
 
 func main() {
 	data, err := load()
 	if err != nil {
 		fmt.Println(err)
+		return
 	}
 	if len(os.Args) > 1 {
 		switch os.Args[1] {
 		case "list":
-			for _, exp := range data.Expenses {
-				fmt.Printf("%-4d %-12s %-5d %s\n", exp.ID,
-					exp.Date.Format("02-01-2006"), exp.Amount, exp.Description)
-			}
+			data.list()
 		case "add":
-			if len(os.Args) != 4 {
-				fmt.Printf("Error: invalid number of arguments, use add \"description\" amount\n")
-			} else {
-				amount, err := strconv.Atoi(os.Args[3])
-				if err != nil {
-					fmt.Printf("Error: incorrect amount format, amount must be integer\n")
-				}
-				description := os.Args[2]
-				add(&data, amount, description)
-				save(data)
-			}
+			data.add()
 		case "summary":
-			var sum int
-			for _, exp := range data.Expenses {
-				sum += exp.Amount
-			}
-			fmt.Printf("Total expences: %d\n", sum)
+			data.summary()
 		case "delete":
-			if len(os.Args) != 3 {
-				fmt.Printf("Error: invalid number of arguments, use delete id\n")
-			} else {
-				id, err := strconv.Atoi(os.Args[2])
-				if err != nil {
-					fmt.Printf("Error: incorrect ID format, ID must be integer\n")
-				}
-				oldLen := len(data.Expenses)
-				delete(&data, id)
-				save(data)
-				if oldLen == len(data.Expenses) {
-					fmt.Printf("No expense with ID %d\n", id)
-				} else {
-					fmt.Printf("Expense deleted successfully\n")
-				}
-			}
+			data.delete()
 		}
 	}
-
 }
